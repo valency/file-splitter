1067b0d9c362989b1deafb25991ddf961ff60eb5
Ye Ding <guiewy@gmail.com>
2015-08-04 20:03:05 +0800

    v0.4.7

---
+++ Keyword Mapper/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Keyword_Mapper {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.keywordfilepath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keywordfilepath
+            // 
+            this.keywordfilepath.AllowDrop = true;
+            this.keywordfilepath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.keywordfilepath.ForeColor = System.Drawing.Color.Green;
+            this.keywordfilepath.Location = new System.Drawing.Point(8, 40);
+            this.keywordfilepath.Name = "keywordfilepath";
+            this.keywordfilepath.Size = new System.Drawing.Size(328, 48);
+            this.keywordfilepath.TabIndex = 0;
+            this.keywordfilepath.Text = "DRAG FILE HERE";
+            this.keywordfilepath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.keywordfilepath.DragDrop += new System.Windows.Forms.DragEventHandler(this.keywordfilepath_DragDrop);
+            this.keywordfilepath.DragEnter += new System.Windows.Forms.DragEventHandler(this.keywordfilepath_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(8, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "READY";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(216, 8);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 27);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(8, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "HOW MANY LINES PER FILE?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 129);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keywordfilepath);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "File Splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label keywordfilepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Keyword Mapper/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Keyword_Mapper {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void keywordfilepath_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void keywordfilepath_DragDrop(object sender, DragEventArgs e) {
+            keywordfilepath.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            StreamReader reader = new StreamReader(keywordfilepath.Text);
+            string s = reader.ReadLine();
+            int linecount = 0;
+            int filecount = 0;
+            StreamWriter writer = new StreamWriter(keywordfilepath.Text.Replace(".txt", "." + linecount + ".txt"));
+            while (s != null) {
+                writer.WriteLine(s);
+                if (linecount % 100 == 0 && linecount != 0) {
+                    filecount++;
+                    label1.Text = "PROCESSING FILE " + filecount + "...";
+                    Application.DoEvents();
+                    writer.Close();
+                    writer = new StreamWriter(keywordfilepath.Text.Replace(".txt", "." + filecount + ".txt"));
+                }
+                s = reader.ReadLine();
+                linecount++;
+            }
+            reader.Close();
+            writer.Close();
+            label1.Text = "ALL DONE.";
+            Application.DoEvents();
+        }
+    }
+}
